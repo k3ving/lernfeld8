cd3b0805a955b646a204952e6759642f41ff8d21
Patrick Winkler <ppuwinkler@gmail.com>
2023-12-14 12:08:30 +0100

    add end

--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var score = 0
 var started = false
+var ended = false
 	
 
 func is_started():
@@ -11,6 +12,7 @@ func is_started():
 func _process(delta):
 	if Input.is_action_pressed("ui_select"):
 		started = true
-	if started:	
+	if started && !ended:	
 		score +=1
+	if ended:
 		print(score)
